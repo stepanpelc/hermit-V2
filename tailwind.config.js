b85754fe1309566e620dea0d04ab5c5bb3aa6da0
Stepan Pelc <stepan.pelc@soft-eng.cz>
2025-06-29 15:56:47 +0200

    tailwind update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./archetypes/**/*.md",
+    "./assets/js/**/*.js",
+    "./assets/scss/**/*.scss"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
